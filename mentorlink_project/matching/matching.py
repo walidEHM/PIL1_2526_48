7a960244f3ffe12b2bba1cdf0dc776e7a77b5444
faridath07 <gabafaridath@gmail.com>
2026-06-05 13:46:52 +0200

    update : mise à jour de l'algorithme de matching

--- mentorlink_project/matching/matching.py
+++ mentorlink_project/matching/matching.py
@@ -1,1 +1,28 @@
+def preparer_competences( competences_mentor,competences_mentore):
+   #Convertir les listes en ensemble pour trouver les élements commun
+   competences_communes =set( competences_mentor) & set(competences_mentore)
 
+   #Compter les competences en commun
+   commun = len( competences_communes)
+
+   #Compter les competences demandées par le mentoré
+   demandees = len ( compentences_mentore)
+
+   return commun, demandees
+  
+def preparer_horaires ( horaires_mentor, horaire_mentore):
+   horaires_communs = set(horaires_mentor) & set( horaires_mentore)
+   crenaux_communs= len ( horaires_communs)
+   crenaux_mentore= len (horaires_mentore)
+   return crenaux_communs , crenaux_mentore
+
+ def calculer_score ( commun, demandees, crenaux_communs, crenaux_mentore, meme_filiere , ecart_niveau):
+   if demandees == 0
+      score_competences = 0
+   else:
+       score_competences = ( commun / demandees) * 100
+   if crenaux_mentore == 0
+       score_horaires = 0
+   else:
+   score_horaires = ( crenaux_communs / crenaux_mentore) * 100
+    
