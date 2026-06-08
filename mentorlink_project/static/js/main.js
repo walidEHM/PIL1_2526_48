9219c3a84a0a021ddc2d139d31ae212b8be982f8
chanelfoly <darrylchanelcomlanfoly@gmail.com>
2026-06-08 02:05:35 +0100

    main.js( Profil)
    Profil

--- mentorlink_project/static/js/main.js
+++ mentorlink_project/static/js/main.js
@@ -1,1 +1,19 @@
+/**
+ * Ouvre la modale de modification du profil
+ */
+function openEdit() { 
+  document.getElementById('editModal').classList.add('show'); 
+}
 
+/**
+ * Ferme la modale de modification du profil
+ */
+function closeEdit() { 
+  document.getElementById('editModal').classList.remove('show'); 
+}
+
+document.getElementById('editModal').addEventListener('click', function(e) {
+  if (e.target === this) {
+    closeEdit();
+  }
+});
