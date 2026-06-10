3598d58e0a58694888f5314097b569a80ba3aa3b
faridath07 <gabafaridath@gmail.com>
2026-06-10 14:12:16 +0100

    Add ModifierProfilForm for user profile updates

--- mentorlink_project/matching/match.py
+++ mentorlink_project/matching/match.py
@@ -116,3 +116,19 @@ def generer_matches_pour_utilisateur(utilisateur):
 
 
 
+
+
+class ModifierProfilForm(forms.ModelForm):
+    class Meta:
+        model = Utilisateur
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'telephone',
+            'niveau',
+            'filiere',
+            'bio',
+            'photo',
+        )
+
